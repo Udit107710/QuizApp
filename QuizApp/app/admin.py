2efe306ed4f89876fb5baf33093c2af92005127a
Udit Krishna Chaudhary <uditcr710107@gmail.com>
2020-07-06 11:37:23 +0530

    The admin is able to edit answers and the question in the same page of the admin site.

--- QuizApp/app/admin.py
+++ QuizApp/app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Question, Answer
 
-# Register your models here.
+class AnswerAdmin(admin.StackedInline):
+    model = Answer
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [AnswerAdmin]
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Answer)
