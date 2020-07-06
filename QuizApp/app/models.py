b8527e1d510dbfad867a0e0c9be97e3edbfeba65
Udit Krishna Chaudhary <uditcr710107@gmail.com>
2020-07-06 10:38:48 +0530

    Initial project with new models.

---
+++ QuizApp/app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Question(models.Model):
+    text = models.CharField(max_length=120)
+
+
+class Answer(models.Model):
+    option = models.CharField(max_length=30)
+
+class QuestionAnswer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
